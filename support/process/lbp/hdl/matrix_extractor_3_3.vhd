f217c2e39210ab6c7484fc13f190bbb523bc4c46
sebcaux <sebastien.caux@robotips.fr>
2015-12-18 17:49:32 +0100

    [Matrix extractor] fixed somes bugs

--- support/process/lbp/hdl/matrix_extractor_3_3.vhd
+++ support/process/lbp/hdl/matrix_extractor_3_3.vhd
@@ -69,6 +69,8 @@ component scfifo
 	);
 end component;
 
+signal enable_reg :		std_logic;
+
 signal x_pos : unsigned(15 downto 0);
 signal y_pos : unsigned(15 downto 0);
 
@@ -76,19 +78,43 @@ signal p00_s, p01_s, p02_s : std_logic_vector((PIX_WIDTH-1) downto 0);
 signal p10_s, p11_s, p12_s : std_logic_vector((PIX_WIDTH-1) downto 0);
 signal p20_s, p21_s, p22_s : std_logic_vector((PIX_WIDTH-1) downto 0);
 
+signal line0_read : std_logic;
+signal line0_write : std_logic;
+signal line0_empty : std_logic;
+signal line0_pix_out : std_logic_vector((PIX_WIDTH-1) downto 0);
+
 signal line1_read : std_logic;
 signal line1_write : std_logic;
 signal line1_empty : std_logic;
 signal line1_pix_out : std_logic_vector((PIX_WIDTH-1) downto 0);
 
-signal line2_read : std_logic;
-signal line2_write : std_logic;
-signal line2_empty : std_logic;
-signal line2_pix_out : std_logic_vector((PIX_WIDTH-1) downto 0);
+signal linedv_read : std_logic;
+signal linedv_write : std_logic;
+signal linedv_empty : std_logic;
 
 signal dummy_dv : std_logic;
+signal out_dv_s : std_logic;
+signal cell : std_logic_vector((LINE_WIDTH_MAX-1) downto 0);
+
+signal matrix_dv_s : std_logic;
 
 begin
+
+	line0_fifo : scfifo
+    generic map (
+    	LPM_WIDTH	=>	PIX_WIDTH,
+    	LPM_WIDTHU	=> FIFO_LENGHT_WIDTH,
+    	LPM_NUMWORDS => FIFO_LENGHT
+	)
+    port map (
+		data => p20_s,
+		clock => clk_proc,
+		wrreq => line0_write,
+		q => p12_s,
+		rdreq => line0_read,
+		aclr => not(reset_n),
+		empty => line0_empty
+    );
 
 	line1_fifo : scfifo
     generic map (
@@ -97,104 +123,97 @@ begin
     	LPM_NUMWORDS => FIFO_LENGHT
 	)
     port map (
-		data => p02_s,
+		data => p10_s,
 		clock => clk_proc,
 		wrreq => line1_write,
-		q => line1_pix_out,
+		q => p02_s,
 		rdreq => line1_read,
 		aclr => not(reset_n),
 		empty => line1_empty
     );
 
-	line2_fifo : scfifo
-    generic map (
-    	LPM_WIDTH	=>	PIX_WIDTH,
-    	LPM_WIDTHU	=> FIFO_LENGHT_WIDTH,
-    	LPM_NUMWORDS => FIFO_LENGHT
-	)
-    port map (
-		data => p12_s,
-		clock => clk_proc,
-		wrreq => line2_write,
-		q => line2_pix_out,
-		rdreq => line2_read,
-		aclr => not(reset_n),
-		empty => line2_empty
-    );
-    
     process (clk_proc, reset_n)
 	begin
 		if(reset_n='0') then
 			x_pos <= to_unsigned(0, 16);
 			y_pos <= to_unsigned(0, 16);
+			line0_read <= '0';
+			line0_write <= '0';
+			line1_read <= '0';
+			line0_write <= '0';
 			
 		elsif(rising_edge(clk_proc)) then
-			matrix_dv <= '0';
-			dummy_dv <= '0';
+			matrix_dv <= matrix_dv_s;
+			dummy_dv <= matrix_dv_s;
+			matrix_dv_s <= '0';
 			if(in_fv='0') then
 				x_pos <= to_unsigned(0, 16);
 				y_pos <= to_unsigned(0, 16);
+				line0_read <= '0';
+				line0_write <= '0';
+				line1_read <= '0';
+				line0_write <= '0';
 
-			elsif(in_dv='1') then
-				x_pos <= x_pos+1;
-				
-				p00_s <= in_data;
-				p01_s <= p00_s;
-				p02_s <= p01_s;
-				
-				p10_s <= line1_pix_out;
-				p11_s <= p10_s;
-				p12_s <= p11_s;
-				
-				p20_s <= line2_pix_out;
-				p21_s <= p20_s;
-				p22_s <= p21_s;
-				
-				if(x_pos<to_unsigned(3, 16)) then
-					line1_write <= '0';
-					line2_write <= '0';
-					out_data <= (others => '0');
-					dummy_dv <= '1';
-				elsif(x_pos=unsigned(widthimg_i)-1) then
-					line1_write <= '0';
-					line2_write <= '0';
-					y_pos <= y_pos+1;
-					x_pos <= to_unsigned(0, 16);
-					out_data <= (others => '0');
-					dummy_dv <= '1';
-				else
-					line1_write <= '1';
-					dummy_dv <= '0';
-					if(y_pos>to_unsigned(1, 16)) then
-						line2_write <= '1';
-						out_data <= value_data;
-						matrix_dv <= '1';
-					else
-						line2_write <= '0';
-						out_data <= (others => '0');
-						dummy_dv <= '1';
-					end if;
+			else
+				if (line0_read='1') then
+					p11_s <= p12_s;
+					p10_s <= p11_s;
+				end if;
+				if (line1_read='1') then
+					p01_s <= p02_s;
+					p00_s <= p01_s;
 				end if;
 				
-				if(y_pos=to_unsigned(0, 16)) then
-					line1_read <= '0';
-					line2_read <= '0';
-				elsif(y_pos>to_unsigned(1, 16)) then
-					line1_read <= '1';
-					line2_read <= '1';
+				if (in_dv='1') then
+					x_pos <= x_pos+1;
+					
+					p22_s <= in_data;
+					p21_s <= p22_s;
+					p20_s <= p21_s;
+					
+					if(x_pos<=to_unsigned(1, 16)) then
+						line0_write <= '0';
+						line1_write <= '0';
+					elsif(x_pos=unsigned(widthimg_i)-1) then
+						line0_write <= '0';
+						line1_write <= '0';
+						y_pos <= y_pos+1;
+						x_pos <= to_unsigned(0, 16);
+					else
+						line0_write <= '1';
+						if(y_pos>to_unsigned(1, 16)) then
+							line1_write <= '1';
+							matrix_dv_s <= '1';
+						else
+							line1_write <= '0';
+						end if;
+					end if;
+					
+					if(y_pos=to_unsigned(0, 16)) then
+						line0_read <= '0';
+						line1_read <= '0';
+					elsif(y_pos>to_unsigned(1, 16)) then
+						line0_read <= '1';
+						line1_read <= '1';
+					else
+						line1_read <= '0';
+						line0_read <= '1';
+					end if;
 				else
-					line1_read <= '1';
-					line2_read <= '0';
+					line0_read <= '0';
+					line0_write <= '0';
+					line1_read <= '0';
+					line0_write <= '0';
 				end if;
 			end if;
 		end if;
 	end process;
-	
-	out_dv <= value_dv or dummy_dv;
-	out_fv <= in_fv;
-	
+
 	p00 <= p00_s; p01 <= p01_s; p02 <= p02_s;
 	p10 <= p10_s; p11 <= p11_s; p12 <= p12_s;
 	p20 <= p20_s; p21 <= p21_s; p22 <= p22_s;
 
+	out_data <= value_data;
+	out_dv   <= value_dv;
+	out_fv   <= in_fv;
 end rtl;
