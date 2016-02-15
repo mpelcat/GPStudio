2bf7a94bd46620ddf5fe917ecd6d788442ae448a
sebcaux <sebastien.caux@robotips.fr>
2016-02-15 09:57:32 +0100

    block_generator bit field read and write

--- support/toolchain/hdl/block_generator.php
+++ support/toolchain/hdl/block_generator.php
@@ -112,13 +112,13 @@ class Block_generator
 							{
 								// port connexion
 								$port_param_in = new Port();
-								$port_param_in->name = $parambitfields->name;
+								$port_param_in->name = $param->name.'_'.$parambitfields->name;
 								$port_param_in->type = "in";
 								$port_param_in->size = $size;
 								$this->process_block->addExtPort($port_param_in);
 								
 								$port_param_out = new Port();
-								$port_param_out->name = $parambitfields->name;
+								$port_param_out->name = $param->name.'_'.$parambitfields->name;
 								$port_param_out->type = "out";
 								$port_param_out->size = $size;
 								$this->slave_block->addExtPort($port_param_out);
@@ -154,7 +154,7 @@ class Block_generator
 							$size = count($parambitfields->bitfieldlist);
 							
 							if($size==1) $type="std_logic"; else $type="std_logic_vector";
-							if($size>0) $this->block_generator->addSignal($parambitfields->name, $size, $type);
+							if($size>0) $this->block_generator->addSignal($param->name.'_'.$parambitfields->name, $size, $type);
 						}
 					}
 				}
@@ -209,21 +209,23 @@ class Block_generator
 				}
 				else
 				{
-					foreach($param->parambitfields as $parambitfields)
+					foreach($param->parambitfields as $parambitfield)
 					{
-						$size = count($parambitfields->bitfieldlist);
+						$size = count($parambitfield->bitfieldlist);
 						
 						if($size>0)
 						{
 							if($size==1) $type="std_logic"; else $type="std_logic_vector";
 							
 							// slave internal
-							$this->slave_generator->addSignal($param->name."_reg", $size, $type);
+							$this->slave_generator->addSignal($param->name."_".$parambitfield->name."_reg", $size, $type);
 						}
 					}
 				}
 			}
 		}
+		
+		$size_master = $this->block->pi_size_addr_rel;
 		
 		// write reg code
 		$code_wr="";
@@ -238,32 +240,39 @@ class Block_generator
 		{
 			if($param->hard==false)
 			{
+				$contant_reg_addr = strtoupper($param->name)."_REG_ADDR";
+				$code_wr.="					when std_logic_vector(to_unsigned(".$contant_reg_addr.", ".$size_master."))=>"."\r\n";
+				
 				if(empty($param->parambitfields))
 				{
 					$size = 32;
-					
-					$contant_reg_addr = strtoupper($param->name)."_REG_ADDR";
 					$reg_name = $param->name."_reg";
-					$size_master = $this->block->pi_size_addr_rel;
-					
-					$code_wr.="					when std_logic_vector(to_unsigned(".$contant_reg_addr.", ".$size_master."))=>"."\r\n";
 					$code_wr.="						".$reg_name." <= datawr_i;"."\r\n";
 				}
 				else
 				{
-					foreach($param->parambitfields as $parambitfields)
+					foreach($param->parambitfields as $parambitfield)
 					{
-						$size = count($parambitfields->bitfieldlist);
+						$size = count($parambitfield->bitfieldlist);
 						
 						if($size>0)
 						{
-							if($size==1) $type="std_logic"; else $type="std_logic_vector";
+							$bf_name = $param->name."_".$parambitfield->name."_reg";
+							
+							$code_wr.="						".$bf_name." <= ";
+							$count=0;
+							foreach($parambitfield->bitfieldlist as $bit)
+							{
+								if($count>0) $code_wr.=" & ";
+								$code_wr.="datawr_i(".$bit.")";
+								$count++;
+							}
+							$code_wr.=";"."\r\n";
 						}
 					}
 				}
 			}
 		}
-		
 		
 		$code_wr.="					when others=>"."\r\n";
 		$code_wr.="				end case;"."\r\n";
@@ -285,28 +294,48 @@ class Block_generator
 		{
 			if($param->hard==false)
 			{
+				$contant_reg_addr = strtoupper($param->name)."_REG_ADDR";
+				$code_rd.="					when std_logic_vector(to_unsigned(".$contant_reg_addr.", ".$size_master."))=>"."\r\n";
+				
 				if(empty($param->parambitfields))
 				{
 					$size = 32;
 					
-					$contant_reg_addr = strtoupper($param->name)."_REG_ADDR";
 					$reg_name = $param->name."_reg";
-					$size_master = $this->block->pi_size_addr_rel;
 					
-					$code_rd.="					when std_logic_vector(to_unsigned(".$contant_reg_addr.", ".$size_master."))=>"."\r\n";
 					$code_rd.="						datard_o <= ".$reg_name.";"."\r\n";
 				}
 				else
 				{
-					foreach($param->parambitfields as $parambitfields)
+					$bits=array();
+					foreach($param->parambitfields as $parambitfield)
 					{
-						$size = count($parambitfields->bitfieldlist);
+						$size = count($parambitfield->bitfieldlist);
 						
-						if($size>0)
+						$bf_name = $param->name."_".$parambitfield->name."_reg";
+						
+						$count=$size;
+						foreach($parambitfield->bitfieldlist as $bit)
 						{
-							if($size==1) $type="std_logic"; else $type="std_logic_vector";
+							$bits[$bit]=$bf_name."(".($count-1).")";
+							$count--;
 						}
 					}
+					
+					$code_rd.="						datard_o <= ";
+					$prev=31;
+					$count=0;
+					ksort($bits);
+					$bits=array_reverse($bits,true);
+					foreach($bits as $bitpos => $bitname)
+					{
+						if($count>0) $code_rd.=" & ";
+						if($bitpos<$prev-1) $code_rd.="\"".str_repeat('0',$prev-$bitpos-1)."\" & ";
+						$code_rd.=$bitname;
+						$prev = $bitpos;
+						$count++;
+					}
+					$code_rd.=";"."\r\n";
 				}
 			}
 		}
@@ -333,7 +362,17 @@ class Block_generator
 				}
 				else
 				{
-					
+					foreach($param->parambitfields as $parambitfield)
+					{
+						$size = count($parambitfield->bitfieldlist);
+						
+						if($size>0)
+						{
+							$bf_name = $param->name."_".$parambitfield->name;
+							
+							$code_connect.="	".$bf_name." <= ".$bf_name."_reg;"."\r\n";
+						}
+					}
 				}
 			}
 		}
@@ -346,7 +385,6 @@ class Block_generator
 		$this->slave_generator->fromBlock($this->slave_block, TRUE);
 		$this->slave_generator->save_as_ifdiff($path.DIRECTORY_SEPARATOR.$this->slave_generator->name.'.vhd');
 	}
-	
 }
 
 ?>
