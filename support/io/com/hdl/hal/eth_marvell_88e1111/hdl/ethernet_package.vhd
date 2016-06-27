efee7c68bcfbe538041a5387ae1f0c007813633a
alsoulier <alexandre.soulier3@gmail.com>
2016-06-27 16:29:17 +0200

    [eth] Add ethernet package

---
+++ support/io/com/hdl/hal/eth_marvell_88e1111/hdl/ethernet_package.vhd
@@ -0,0 +1,26 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+package ethernet_package is 
+	
+	 type rgmii_t is record
+		data 	: std_logic_vector(3 downto 0);
+		dv		: std_logic;
+    end record;
+	 
+	 type gmii_t is record
+	    data		: std_logic_vector(7 downto 0);
+	    dv		: std_logic;
+    end record;
+	 
+	 type flag is record
+        good		: std_logic;
+        bad		: std_logic;
+    end record;
+end ethernet_package;
+
+
+
+
+
