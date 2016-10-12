8a387b285f490acf6bd66620cdbca1324659fffb
hened <edouard.hendrickx@gmail.com>
2016-10-12 11:13:29 +0200

    [sobel] added sobel ip

---
+++ support/process/sobel/hdl/sobel.vhd
@@ -0,0 +1,130 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.all;
+use IEEE.NUMERIC_STD.all;
+library std;
+
+entity sobel is
+	generic (
+		LINE_WIDTH_MAX : integer;
+		CLK_PROC_FREQ  : integer;
+		IN_SIZE        : integer;
+		OUT_SIZE       : integer
+	);
+	port (
+		clk_proc   : in std_logic;
+		reset_n    : in std_logic;
+
+		------------------------- in flow -----------------------
+		in_data    : in std_logic_vector(IN_SIZE-1 downto 0);
+		in_fv      : in std_logic;
+		in_dv      : in std_logic;
+
+		------------------------ out flow -----------------------
+		out_data   : out std_logic_vector(OUT_SIZE-1 downto 0);
+		out_fv     : out std_logic;
+		out_dv     : out std_logic;
+
+		--======================= Slaves ========================
+
+		------------------------- bus_sl ------------------------
+		addr_rel_i : in std_logic_vector(3 downto 0);
+		wr_i       : in std_logic;
+		rd_i       : in std_logic;
+		datawr_i   : in std_logic_vector(31 downto 0);
+		datard_o   : out std_logic_vector(31 downto 0)
+	);
+end sobel;
+
+architecture rtl of sobel is
+component sobel_process
+	generic (
+		LINE_WIDTH_MAX : integer;
+		CLK_PROC_FREQ  : integer;
+		IN_SIZE        : integer;
+		OUT_SIZE       : integer
+	);
+	port (
+		clk_proc              : in std_logic;
+		reset_n               : in std_logic;
+
+		---------------- dynamic parameters ports ---------------
+		status_reg_enable_bit : in std_logic;
+		widthimg_reg_width    : in std_logic_vector(15 downto 0);
+
+		------------------------- in flow -----------------------
+		in_data               : in std_logic_vector(IN_SIZE-1 downto 0);
+		in_fv                 : in std_logic;
+		in_dv                 : in std_logic;
+
+		------------------------ out flow -----------------------
+		out_data              : out std_logic_vector(OUT_SIZE-1 downto 0);
+		out_fv                : out std_logic;
+		out_dv                : out std_logic
+	);
+end component;
+
+component sobel_slave
+	generic (
+		CLK_PROC_FREQ : integer
+	);
+	port (
+		clk_proc              : in std_logic;
+		reset_n               : in std_logic;
+
+		---------------- dynamic parameters ports ---------------
+		status_reg_enable_bit : out std_logic;
+		widthimg_reg_width    : out std_logic_vector(15 downto 0);
+
+		--======================= Slaves ========================
+
+		------------------------- bus_sl ------------------------
+		addr_rel_i            : in std_logic_vector(3 downto 0);
+		wr_i                  : in std_logic;
+		rd_i                  : in std_logic;
+		datawr_i              : in std_logic_vector(31 downto 0);
+		datard_o              : out std_logic_vector(31 downto 0)
+	);
+end component;
+
+	signal status_reg_enable_bit : std_logic;
+	signal widthimg_reg_width    : std_logic_vector (15 downto 0);
+
+begin
+	sobel_process_inst : sobel_process
+    generic map (
+		CLK_PROC_FREQ  => CLK_PROC_FREQ,
+		LINE_WIDTH_MAX => LINE_WIDTH_MAX,
+		IN_SIZE        => IN_SIZE,
+		OUT_SIZE       => OUT_SIZE
+	)
+    port map (
+		clk_proc              => clk_proc,
+		reset_n               => reset_n,
+		status_reg_enable_bit => status_reg_enable_bit,
+		widthimg_reg_width    => widthimg_reg_width,
+		in_data               => in_data,
+		in_fv                 => in_fv,
+		in_dv                 => in_dv,
+		out_data              => out_data,
+		out_fv                => out_fv,
+		out_dv                => out_dv
+	);
+
+	sobel_slave_inst : sobel_slave
+    generic map (
+		CLK_PROC_FREQ => CLK_PROC_FREQ
+	)
+    port map (
+		clk_proc              => clk_proc,
+		reset_n               => reset_n,
+		status_reg_enable_bit => status_reg_enable_bit,
+		widthimg_reg_width    => widthimg_reg_width,
+		addr_rel_i            => addr_rel_i,
+		wr_i                  => wr_i,
+		rd_i                  => rd_i,
+		datawr_i              => datawr_i,
+		datard_o              => datard_o
+	);
+
+
+end rtl;
